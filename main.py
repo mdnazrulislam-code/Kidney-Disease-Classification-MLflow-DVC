d645cde960cc2c341b2860df1d7216cd9f88273d
Md Nazrul Islam <arfanislamabir@gmail.com>
2023-10-08 21:59:18 +0600

    Data Ingestion is completed and moduler code is implemented

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welocme to our custom log without src")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<<<<<<< \n\nX==============X")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
